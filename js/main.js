be3b2b497fd6eed6a2380602e2b70c5ffa44aa24
voliseq <maciej.radziszowski@gmail.com>
2017-05-07 04:51:29 +0200

    base agds strucure done

--- js/main.js
+++ js/main.js
@@ -1,31 +1,14 @@
 ((w) => {
 
-    let agds = w.agds;
+    let agds = w.agds,
+        graph = {};
 
+    iris = agds.commaToDot(iris);
 
+    graph.param = agds.createMainNode(iris);
+    console.log(graph.param);
+    agds.fillPropertiesWithObjects(graph.param, iris);
 
-    let leaf_length = agds.propertyValues(iris, "leaf-length"),
-        leaf_width = agds.propertyValues(iris, "leaf-width"),
-        petal_length = agds.propertyValues(iris, "petal-length"),
-        petal_width = agds.propertyValues(iris, "petal-width"),
-        classes = agds.getClasses(iris);
-
-
-
-    let graph = {
-        param: {
-            "leaf-length": leaf_length,
-            "leaf-width": leaf_width,
-            "petal-length": petal_length,
-            "petal-width": petal_width
-        }
-    };
-
-    console.log(graph);
-
-    leaf_length.map(object => {
-        let key = Object.keys(object)[0];
-    })
 
 
 
--- js/plugins.js
+++ js/plugins.js
@@ -1,6 +1,53 @@
 ((w) => {
 
     w.agds = w.agds || {};
+
+    w.agds.commaToDot = (objects) => {
+        return objects.map(object => {
+            for (property in object) {
+                object[property] = object[property].replace(",", ".");
+            }
+            return object;
+        });
+
+    };
+
+    w.agds.createMainNode = (objects) => {
+
+        let main_node = {},
+            properties = Object.keys(objects[0]);
+
+        properties.map((property) => {
+            main_node[property] = w.agds.propertyValues(objects, property);
+        });
+
+        return main_node;
+
+    };
+
+    w.agds.fillPropertiesWithObjects = (graph_object, objects) => {
+        let properties = Object.keys(graph_object),
+            key;
+
+        for(property in graph_object){
+            graph_object[property].map(node => {
+                key = Object.keys(node)[0];
+                node[key] = objects.filter(object => {
+                    return object[property] == key;
+                })
+            })
+        }
+
+        console.log(graph_object);
+
+    };
+
+    w.agds.getObjectsWithPropertyValue = ((objects, property, value) => {
+        return objects.filter((object) => {
+            return object[property] == value
+        })
+    });
+
     w.agds.propertyValues = (objects, property) => {
         let propArr = objects.map((object) => {
             return object[property];
@@ -14,23 +61,12 @@
             return {
                 [key]: []
             }
-        })
+        });
 
         return propArr;
 
     };
 
-    w.agds.getClasses = (objects) => {
-        return [... new Set(objects.map(object => {
-            return object['class'];
-        }))]
-    };
-
-    w.agds.getObjectsWithPropertyValue = ((objects, property, value) => {
-        return objects.map((object) => {
-            return object[property] == value
-        })
-    })
 
 
 
